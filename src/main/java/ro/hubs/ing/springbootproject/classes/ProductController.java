2eee59ad8fc17dac33d7ef874a8ecb05b10648a4
Iordache Ana-Maria <anaiordache264@gmail.com>
2022-09-18 23:00:24 +0300

    added get, post, put and delete methods

--- src/main/java/ro/hubs/ing/springbootproject/classes/ProductController.java
+++ src/main/java/ro/hubs/ing/springbootproject/classes/ProductController.java
@@ -1,8 +1,15 @@
 package ro.hubs.ing.springbootproject.classes;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -14,4 +21,23 @@ public class ProductController {
         return "Hi admin!";
     }
 
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping(value = "/products")
+    public void addProduct(@RequestBody Product product) {
+        productService.addProduct(product);
+    }
+
+    @PutMapping(value = "/products/{id}")
+    public void updateProduct(@PathVariable int id, @RequestBody Product product) {
+        productService.updateProduct(id, product);
+    }
+
+    @DeleteMapping(value = "/products/{id}")
+    public void deleteProduct(@PathVariable int id) {
+        productService.deleteProduct(id);
+    }
 }
